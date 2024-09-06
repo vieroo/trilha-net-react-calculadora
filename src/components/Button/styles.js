405bedd1a0eefab54de8a39fdb15a06e61dae871
Eduardo Viero <duduviero@gmail.com>
2024-09-06 17:36:30 -0300

    first commit

---
+++ src/components/Button/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+  padding: 20px;
+  border: 1px solid #c0c0cc;
+  background-color: #00aaf0;
+  color: white;
+  font-size: 24px;
+  font-weight: 700;
+  flex: 1;
+
+  &:hover {
+    opacity: 0.6;
+  }
+`;
